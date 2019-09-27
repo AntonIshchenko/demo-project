62e58ece2ba01955de715bbe2ccb775bb3ec463f
Anton Ishchenko <anton.truthful@gmail.com>
2019-09-27 22:59:14 +0400

    Try to parse txt file

--- version0/src/MainClass.java
+++ version0/src/MainClass.java
@@ -1,5 +1,28 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MainClass {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        List<String> fullData = new ArrayList<>();
+        String row = "";
+
+        try(LineNumberReader reader =new LineNumberReader(new BufferedReader(new FileReader("test.txt"))) ){
+            while (row != null) {
+                row = reader.readLine();
+                if(row != null)
+                    fullData.add(row.trim());
+            }
+            fullData.remove(fullData.size()-1);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (String d:fullData) {
+            System.out.println(d);
+        }
+
     }
 }
