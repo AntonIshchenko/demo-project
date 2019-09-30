e65d8b4d92a1bed4dc22a01ad9a159fb5d13006c
Anton Ishchenko <anton_ishchenko@epam.com>
2019-09-30 13:54:42 +0300

    CopybookScanner written bun not tested

--- version0/src/CopybookScanner.java
+++ version0/src/CopybookScanner.java
@@ -1,4 +1,5 @@
 import java.io.DataInputStream;
+import java.io.IOException;
 import java.io.LineNumberReader;
 import java.util.Iterator;
 
@@ -6,6 +7,7 @@ public class CopybookScanner implements Iterator {
     LineNumberReader currentFile;
     DataInputStream currentDataStream;
     Copybook copybookLine = new Copybook();
+
     CopybookScanner(LineNumberReader inputFile, DataInputStream inputStream){
         currentDataStream = inputStream;
         currentFile = inputFile;
@@ -13,11 +15,51 @@ public class CopybookScanner implements Iterator {
 
     @Override
     public boolean hasNext() {
-        return false;
+        String line = "";
+        LineNumberReader templateFile = currentFile;
+        while(true) {
+            try {
+                line = templateFile.readLine();
+            } catch (IOException e) {
+                return false;
+            }
+            if (line == null)
+                return false;
+            CopybookParser.parseCopybook(line);
+            copybookLine = CopybookParser.copybook;
+            if (copybookLine.getValueLength() != 0)
+                return true;
+        }
+    }
+
+    private boolean hasNext(LineNumberReader inputFile) {
+        String line = "";
+        while(true) {
+            try {
+                line = inputFile.readLine();
+            } catch (IOException e) {
+                return false;
+            }
+            if (line == null)
+                return false;
+            CopybookParser.parseCopybook(line);
+            copybookLine = CopybookParser.copybook;
+            if (copybookLine.getValueLength() != 0)
+                return true;
+        }
     }
 
     @Override
     public Object next() {
-        return null;
+        if(this.hasNext(currentFile)){
+            int bytesToRead = copybookLine.getValueLength();
+            byte[] result = new byte[bytesToRead];
+            try {
+                return currentDataStream.read(result,0,bytesToRead);
+            } catch (IOException e) {
+                return null;
+            }
+        } else
+            return null;
     }
 }
